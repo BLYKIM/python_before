aa40bd321e14cd704d3a87910ba55cf4b3022331
BLYKIM <vkfksql1@gmail.com>
2021-11-10 14:15:48 +0900

    y를 x로썼네 스바

--- udlr.py
+++ udlr.py
@@ -1,7 +1,6 @@
 n = int(input())
 plans = input().split()
-x = 1
-y = 1
+x, y = 1, 1
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
@@ -11,10 +10,9 @@ for plan in plans:
     for i in range(len(moves)):
         if plan == moves[i]:
             nx = x + dx[i]
-            ny = x + dy[i]
+            ny = y + dy[i]
     if nx < 1 or ny < 1 or nx > n or ny > n:
         continue
-    x = nx
-    y = ny
+    x, y = nx, ny
 
 print(x, y)
